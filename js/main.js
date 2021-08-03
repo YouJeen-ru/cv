4cc8c875317e4b2858a72da02d6bf853a0d47b93
YouJeen-ru <1.rouch@mail.ru>
2021-08-03 18:38:29 +0300

    arrow scroll top completed

--- js/main.js
+++ js/main.js
@@ -40,3 +40,9 @@ function scrollActive() {
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
